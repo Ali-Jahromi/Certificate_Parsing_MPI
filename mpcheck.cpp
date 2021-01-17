438af1be12c477f2ebb6ae0b23a854431b9ea02c
Ali-Jahromi <alisa7420@hotmail.com>
2021-01-17 01:20:37 -0800

    Extracting certificate Issuers and Subjects from certificate files.

--- mpcheck.cpp
+++ mpcheck.cpp
@@ -2,6 +2,7 @@
 #include "mpi.h"
 #include <openssl/x509.h>
 #include <openssl/x509v3.h>
+#include <openssl/pem.h>
 
 using namespace std;
 
@@ -15,7 +16,30 @@ int main(int argc, char* argv[]){
 	string fname(argv[1]);	
 	fname += "." + to_string(mpirank);
 	cout << "Rank #" << mpirank << " reading " << fname << endl;
+	
+	// Openning and Reading the Input files
+	FILE *fp = fopen(fname.c_str(), "r");
+	if (!fp) {
+			fprintf(stderr, "unable to open: %s\n", fname.c_str());
+				return EXIT_FAILURE;
+	}
+	
+	X509 *cert = PEM_read_X509(fp, NULL, NULL, NULL);
+	if (!cert) {
+			fprintf(stderr, "unable to parse certificate in: %s\n", fname.c_str());
+				fclose(fp);
+					return EXIT_FAILURE;
+	}
 
+	// Extracting certificate subject
+        char *subj = X509_NAME_oneline(X509_get_subject_name(cert), NULL, 0);	
+	// Extracging certificate issuer
+	char *issuer = X509_NAME_oneline(X509_get_issuer_name(cert), NULL, 0);
+
+	cout << "File " << fname << " ==> Subject: " << subj << endl << 
+		"Issuer: " << issuer << endl;
+	X509_free(cert);
+	fclose(fp);
 
 
 
